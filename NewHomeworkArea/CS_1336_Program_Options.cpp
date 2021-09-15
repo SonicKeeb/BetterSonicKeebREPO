699e1f27c717c1f84ad0ac9256e3cc86240e84b3
Hamilton Ngo <hamiltonngo@Hamiltons-MacBook-Air.local>
2021-09-15 11:53:10 -0500

    Code for CS 1336 Program Options

---
+++ NewHomeworkArea/CS_1336_Program_Options.cpp
@@ -0,0 +1,39 @@
+//
+//  HomeworkWORK.cpp
+//  NewHomeworkArea
+//
+//  Created by Hamilton Ngo on 9/15/21.
+//
+
+#include "HomeworkWORK.hpp"
+#include <iostream>
+
+int main() {
+    char optionFlag;
+    std::cin >> optionFlag;
+
+    // Put the switch statement here
+    
+    switch (optionFlag) {
+        case 'a':
+            std::cout << "Everything" << std::endl;
+            break;
+        case 'c':
+            std::cout << "Character mode" << std::endl;
+            break;;
+        case 'e':
+            std::cout << "Everything" << std::endl;
+            break;;
+        case 'i':
+            std::cout << "Interactive" << std::endl;
+            break;
+        case 'o':
+            std::cout << "Printing to a file" << std::endl;
+            break;
+        default:
+            std::cout << "Not supported" << std::endl;
+            break;
+    }
+    return 0;
+}
+
