caf9ecaf566b5e66774dbf53a06d006e0760a8fc
Hamilton Ngo <hamiltonngo@cometnet-10-21-2-110.utdallas.edu>
2021-09-15 13:13:51 -0500

    Added Comments for Income Tax Code

--- NewHomeworkArea/CS 1336 Income Tax Reform/CS 1336 Income Tax Code.cpp
+++ NewHomeworkArea/CS 1336 Income Tax Reform/CS 1336 Income Tax Code.cpp
@@ -8,10 +8,11 @@
 #include "CS 1336 Income Tax Code.hpp"
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 int main() {
 
+    // Setting constants for the married and single status expense and declaring all other variables
+    
     const int deductionMarried = 24000;
     const int deductionSingle = 12000;
     int wages;
@@ -25,32 +26,47 @@ int main() {
     int taxDue;
     double federalTax;
    
-    cin >> wages >> interest >> unemployment >> status >> taxes;
+    // CIN
+    
+    std::cin >> wages >> interest >> unemployment >> status >> taxes;
+    
+    // settings variable agi = to what it is / calculation
+    
     agi = wages + interest + unemployment;
    
-    cout << "AGI: $" << agi << endl;
+    std::cout << "AGI: $" << agi << endl;
    
+    // Predefining at the start of the code that if agi is ever > 120000 it'll output the error message and stop outputting anything else after that because of return -1
+    
    if (agi > 120000){
-      cout << "Error: Income too high to use this form" << endl;
+      std::cout << "Error: Income too high to use this form" << std::endl;
       return - 1;
    }
    
+    /* Declaring deduction rates based on whether or not the status is a single or married, if the status is 1 then we know to use the constant of single which is 12000 and if it is anything else but 1 and 2 it'll always use the constant of married */
+    
    if (status == 1 ||(status != 1 && status != 2)){
       deduction = deductionSingle;
-   } else{
+   } else {
       deduction = deductionMarried;
    }
    
-   cout << "Deduction: $" << deduction << endl;
+   std::cout << "Deduction: $" << deduction << std::endl;
    
+    // Declaring the computation and defining income
+    
    income = agi - deduction;
    
+    // Setting income to always output 0 if income is ever not greater than 0
+    
    if (income < 0){
       income = 0;
    }
    
-   cout << "Taxable income: $" << income << endl;
+   std::cout << "Taxable income: $" << income << std::endl;
    
+    // The following next lines of code calculate the rate of the Federal tax: depending on the parameters of income and status
+    
    if (deduction == deductionMarried){
       if (income > 80000){
          federalTax = round(9200 + 0.22 * (abs(80000 - income)));
@@ -71,14 +87,18 @@ int main() {
       }
    }
    
-   cout << "Federal tax: $" << federalTax << endl;
+   std::cout << "Federal tax: $" << federalTax << std::endl;
    
+    // Declaring taxDue as being federaltax - taxes
+    
    taxDue = federalTax - taxes;
    
-   if (taxDue < 0){
-      cout << "Tax refund: $" << abs(taxDue) << endl;
-   }else {
-      cout << "Taxes Owed: $" << federalTax << endl;
+    // If tax is ever less than 0 then we return the same value because that is a refund, if not then it is owed and we output the federal tax
+    
+    if (taxDue < 0){
+      std::cout << "Tax refund: $" << abs(taxDue) << std::endl;
+       } else {
+      std::cout << "Taxes Owed: $" << federalTax << std::endl;
    }
    
    return 0;
