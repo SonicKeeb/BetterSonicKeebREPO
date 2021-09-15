82e525d969cfd9ae38ee3c6c4a2cea9df9701ea5
Hamilton Ngo <hamiltonngo@Hamiltons-MacBook-Air.local>
2021-09-15 11:39:37 -0500

    Hello World

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HomWork Practice Area
+//
+//  Created by Hamilton Ngo on 9/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "By World";
+    return 0;
+}
