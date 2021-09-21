6eafc290ab1bba3048c880ba8972a90667258cfe
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-21 11:28:51 -0500

    Uploading Bonus calculating pi mathmatically

--- NewHomeworkArea/Bonus_CS1336_CalculatePi/Bonus_CS1336_CalculatePi/main.cpp
+++ NewHomeworkArea/Bonus_CS1336_CalculatePi/Bonus_CS1336_CalculatePi/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
-
+using  namespace std;
 int main() {
     
-    
+//Calculate pi mathmatically 
+
+double pi;
+double circumference;
+double diameter;
+
+cout << "Enter the circumference" << endl;
+  cin >> circumference;
+cout << "Enter the diameter" << endl;
+  cin >> diameter;
+
+pi = circumference / diameter;
+
+cout << "Pi is " << pi << endl;
+
+return 0;
     
 }
