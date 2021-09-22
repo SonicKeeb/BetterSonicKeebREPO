c012f9da37409dea70b40afeba58ece32ada8abb
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-22 00:38:30 -0500

    Sending Part 2 up of 6.1Lab

---
+++ NewHomeworkArea/CS1136_6.1LabLesson3/6.1PartTwo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+// This program takes in the input of any amount of dollars and then converts them into Yen, Euros, and Pesos
+
+int main () {
+
+// Delcaration of dollars and the constant Conversion values for the othe currencies
+
+double dollars;
+const double pesos = 19.73;
+const double euros = 0.84;
+const double yen = 113.27;
+
+// Outputting Prompt for the read
+
+cout << "Enter the amount in dollars" << endl;
+
+// Inputting dollars
+
+cin >> dollars;
+
+// Outputting the first table string of the currency names
+// Outputting the bottom row of the table with the converted currencies and set the decimal place to two places
+
+cout << "        Dollars            Yen          Euros          Pesos" << endl;
+cout << "         " << fixed << setprecision(2) << dollars << "       " << (dollars * yen) << "          " << (euros * dollars) << "        " << (pesos * dollars) << endl;
+
+return 0;
+
+}
