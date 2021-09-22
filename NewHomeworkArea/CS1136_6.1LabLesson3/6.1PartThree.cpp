bae46f6dc8d62268fc9e70a3bfaaca035b418842
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-22 09:23:54 -0500

    Fixed calculation issue as well as added comments and fixed spacing

--- NewHomeworkArea/CS1136_6.1LabLesson3/6.1PartThree.cpp
+++ NewHomeworkArea/CS1136_6.1LabLesson3/6.1PartThree.cpp
@@ -3,7 +3,15 @@
 #include <cmath>
 using namespace std;
 
+/*
+This program reads in a companies name and calculaties the stocks
+total purchase price and selling prie based on the price of 
+purchase and sale and amount of stocks owned
+*/
+
 int main() {
+
+// Declaration of all Variables and their types
 
 string companyName;
 int stocks;
@@ -14,6 +22,8 @@ double costOfStock;
 double totalCost;
 double incomeFromStock;
 double totalIncome;
+
+// Ouputting the promps and inputting data of company name, stocks, purchase & selling price
 
 cout << "Enter the name of the company" << endl;
 getline(cin, companyName);
@@ -29,26 +39,30 @@ cin >> salePrice;
 
 cout << endl;
 
+// Intializing variables
+
 costOfStock = stocks * purchasePrice; 
 totalCost = (commission * costOfStock) + costOfStock;
 incomeFromStock = salePrice * stocks;
 totalIncome = incomeFromStock - (commission * incomeFromStock);
 
+// Outputting the previous inputs and categorizing them as well as performing small calculations
+
 cout << "Company: " << companyName << endl;
 cout << "Shares: " << stocks << endl;
-cout << endl << endl;
+cout << endl;
 
-cout << "Purchase//share: $" << fixed << setprecision(2) << purchasePrice << endl;
+cout << "Purchase/share: $" << fixed << setprecision(2) << purchasePrice << endl;
 cout << "Cost of stock: $" << fixed << setprecision(2) << costOfStock << endl;
 cout << "Cost of commission: $" << fixed << setprecision(2) <<  (commission * costOfStock) << endl;
 cout << "Total cost: $" << fixed << setprecision(2) << totalCost << endl;
-cout << endl << endl;
+cout << endl;
 
 cout << "Sale/share: $" << fixed << setprecision(2) <<  salePrice << endl;
 cout << "Income from stock: $" << fixed << setprecision(2) << incomeFromStock << endl;
 cout << "Cost of commission: $" << fixed << setprecision(2) << (commission * incomeFromStock) << endl;
 cout << "Total income: $" << fixed << setprecision(2) << totalIncome << endl;
-cout << endl << endl;
+cout << endl;
 
 cout << "Gain or loss: $" << totalIncome - totalCost << endl;
 
