8635550a297264e79f41155467ca7b66463f86e4
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-22 00:19:08 -0500

    Added comments throughout code

--- NewHomeworkArea/CS1136_6.1LabLesson3/6.1PartOne.cpp
+++ NewHomeworkArea/CS1136_6.1LabLesson3/6.1PartOne.cpp
@@ -9,7 +9,11 @@ The prompt to read in the 5 values must be:
 
 Enter 5 test grades */
 
+// Program takes the input of 5 test scores which are double in type and finds their total average
+
 int main() {
+
+// Declaration of variables in type double
 
 double testGrade;
 double testGrade1;
@@ -18,12 +22,19 @@ double testGrade3;
 double testGrade4;
 double average;
 
+// Outputting prompt to read in the 5 values
 
 cout << "Enter 5 test grades" << endl;
 
+// Next line takes the character inputs for all 5 test grades
+
 cin >> testGrade >> testGrade1 >> testGrade2 >> testGrade3 >> testGrade4;
 
+// Next line defines and initiliazes what average is
+
 average = (testGrade + testGrade1 + testGrade2 + testGrade3 + testGrade4) / 5.0;
+
+// Last line of code outputs the string "Average :" and outputs what the value of the average actually is with 1 decimal place
 
 cout << "Average: " << fixed << setprecision(1) << average << endl;
 
