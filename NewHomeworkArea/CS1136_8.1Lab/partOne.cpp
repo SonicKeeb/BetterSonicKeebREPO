daa7b712461a360ffd9703dd63e21ea9b396ced2
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-25 18:25:08 -0500

    Added comments throughout program

--- NewHomeworkArea/CS1136_8.1Lab/partOne.cpp
+++ NewHomeworkArea/CS1136_8.1Lab/partOne.cpp
@@ -1,3 +1,6 @@
+//This program takes in a user's inputted mass in Kilograms and calculates
+// the weight in (N) of Earth, Moon, Venus.
+
 #include <iostream>
 #include <iomanip>
 #include <cmath>
@@ -5,6 +8,8 @@
 using namespace std;
 
 int main() {
+
+//Declaration of variables and types
 
 double mass;
 const double earthAccel = 9.81;
@@ -14,16 +19,24 @@ double earthWeight;
 double moonWeight;
 double venusWeight; 
 
+//Outputting prompt statements, followed up by user input
+
 cout << "Enter the mass in kg" << endl;
 cin >> mass;
 cout << endl;
 cout << "The mass is " << mass << " kg" << endl;
 
+//Outputting location and weight prompt
+
 cout << "Location" << setw(14) << "Weight (N)" << endl;
+
+//Inititalization of variables and what they are to become
 
 earthWeight = mass * earthAccel;
 moonWeight = mass * moonAccel;
 venusWeight = mass * venusAccel;
+
+//Final output of prompt statements and calculations
 
 cout << "Earth" <<  setw(17) << fixed << setprecision(4) << earthWeight << endl;
 cout << "Moon" << setw(18) << fixed << setprecision(4) << moonWeight << endl;
