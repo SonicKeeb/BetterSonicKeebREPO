faf3a17265507e0200c46ba7288abe49cce5d66e
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-28 12:20:45 -0500

    Added Comments

--- NewHomeworkArea/CS1200_HW2/salaryProblem.cpp
+++ NewHomeworkArea/CS1200_HW2/salaryProblem.cpp
@@ -5,11 +5,20 @@ using namespace std;
 
 int main() {
 
+//Declaring variables
+
 int employees, hoursWorked;
 double payRate, grossPay;
 
+//Outputting Total Employees prompt and inputting total employees
+
 cout << "Total Employees" << endl;
 cin >> employees;
+
+//Loop that iterates depening on total # of employees
+//Takes input of payRate and hoursWorked for each emplyee
+//And calculates it each time for each employee
+//It then outputs the Employee number and their grosspay
 
 for (int i = 0; i <  employees; i++) {
 
