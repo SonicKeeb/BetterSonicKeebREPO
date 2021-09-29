1e77b51519a310be97fd36b12e7afb1a14e1626c
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-09-29 11:22:55 -0500

    Finished first draft of code

---
+++ NewHomeworkArea/CS1136_8.1Lab/lesson4part2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+//This program will read inthe number of seconds and convert it to days, hours, minutes, and remaining seconds.
+
+int main() {
+
+long long int seconds, hours, minutes, days;
+
+cout << "Enter seconds" << endl;
+cin >> seconds;
+cout << "Total seconds: " << seconds << endl << endl;
+
+days = seconds / 86400;
+hours = (seconds % 86400) / 3600;
+seconds = (seconds % 86400) % 3600;
+minutes = seconds / 60;
+seconds = seconds % 60;
+
+if (seconds <= 0) {
+	cout << "Total seconds must be greater than zero" << endl;
+	return -1;
+} else { 
+	;
+}
+
+if (seconds > 0) {
+	if (days == 0) {
+		;
+	} else { 
+	cout << days << " day(s)" << endl;
+	}
+	if (hours == 0) {
+		;
+	} else {
+	cout << hours << " hour(s)" << endl;
+	}
+	if (minutes == 0) {
+		;
+	} else {
+	cout << minutes << " minute(s)" << endl;
+	}
+	if (seconds == 0) {
+		;
+	} else {
+	cout << seconds << " second(s)" << endl;
+	}
+}
+return 0;
+}
