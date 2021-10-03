c9b6e5b019220bd61f7caf6e6ea8ba056c0dfa37
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-10-03 14:23:38 -0500

    Fixed output format and line spacing

--- NewHomeworkArea/CS1336_HailstoneSequence/main.cpp
+++ NewHomeworkArea/CS1336_HailstoneSequence/main.cpp
@@ -4,19 +4,29 @@ using namespace std;
 int main() {
 
    /* Type your code here. */
+
 int inputNumber;
+int count = 0;
+int num = 0;
 
 cin >> inputNumber;
-cout << inputNumber << "\t";
 
 while (inputNumber!= 1) {
+count++;
+cout << inputNumber << "\t";
+if (inputNumber / inputNumber == 1) {
+inputNumber = inputNumber;
 if (inputNumber % 2 != 0) {
 inputNumber = (inputNumber * 3) + 1;
 } else {
 inputNumber = inputNumber / 2;
 }
-cout << inputNumber << "\t";
 }
+if (count % 10 == 0) {
+cout << endl;
+}
+}
+cout << inputNumber;
 cout << endl;
    return 0;
 }
