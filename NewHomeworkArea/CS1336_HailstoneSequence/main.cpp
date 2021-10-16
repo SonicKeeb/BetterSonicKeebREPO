663b7fc23db97f19f7f846c3d5c82e8d280e2741
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-10-15 21:34:41 -0500

    Fixed some indentation

--- NewHomeworkArea/CS1336_HailstoneSequence/main.cpp
+++ NewHomeworkArea/CS1336_HailstoneSequence/main.cpp
@@ -13,25 +13,26 @@ long long int count = 0;
 cin >> inputNumber;
 
 while (inputNumber!= 1) {
-count++;
-if (inputNumber % 2 != 0) {
-cout << RED << inputNumber << "\t";
-} else {
-cout << GREEN << inputNumber << "\t";
-}
-if (inputNumber / inputNumber == 1) {
-inputNumber = inputNumber;
-if (inputNumber % 2 != 0) {
-inputNumber = (inputNumber * 3) + 1;
-} else {
-inputNumber = inputNumber / 2;
-}
-}
-if (count % 10 == 0) {
-cout << endl;
-}
-}
-cout << RED << inputNumber;
-cout << endl;
-   return 0;
+  count++; 
+  if (inputNumber % 2 != 0) { 
+    cout << RED << inputNumber << "\t"; 
+    } else { 
+      cout << GREEN << inputNumber << "\t";
+    } 
+    if (inputNumber / inputNumber == 1) { 
+      inputNumber = inputNumber; 
+      if (inputNumber % 2 != 0) { 
+        inputNumber = (inputNumber * 3) + 1; 
+        } else { 
+          inputNumber = inputNumber / 2; 
+          }
+    } 
+    if (count % 10 == 0) { 
+      cout << endl; 
+      } 
+      } 
+      cout << RED << inputNumber; 
+      cout << endl; 
+
+      return 0;
 }
