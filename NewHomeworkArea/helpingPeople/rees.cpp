57ad99b5b2d2d30afa18f8520b3d177aee8a1618
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-11-05 15:56:30 -0500

    finished first draft

---
+++ NewHomeworkArea/helpingPeople/rees.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+//double average (int& test1, int& test2, int& test3);
+//char getLetterGrade (double avg, int test3);
+//bool getScore (int& testScore, int& testScore, int& testScore3);
+
+double average (int& test1, int& test2, int& test3) {
+
+  cout << "Enter first test score: ";
+  cin >> test1;
+
+  cout << "Enter second test score: ";
+  cin >> test2;
+
+  cout << "Enter third test score: ";
+  cin >> test3;
+
+  cout << fixed << showpoint << setprecision(1);
+
+  double average;
+  average = (test1 + test2 + test3) / 3.0;
+
+  return average;
+}
+
+char getLetterGrade (double avg, int test3) {
+  char grade;
+
+  if (avg >= 90) {
+    grade = 'A';
+  }
+
+  if (avg >= 80 && avg < 90) {
+    if (test3 >= 95) {
+      grade = 'A';
+    } else {
+      grade = 'B';
+    }
+  }
+
+  if (avg >= 70 && avg < 80) {
+    if (test3 >= 85) {
+      grade = 'B';
+    } else {
+      grade = 'C';
+    }
+  }
+
+  if (avg >= 60 && avg < 70) {
+    if (test3 >= 75) {
+      grade = 'C';
+    } else {
+      grade = 'D';
+    }
+  }
+  return grade;
+}
+
+bool getScore () {
+  bool valid;
+  int testScore;
+
+  cout << "Enter testscore: " << endl;
+  cin >> testScore;
+
+  if (testScore < 0 || testScore > 100) {
+    cout << "The score entered is invalid, the valid range is 0 - 100" << endl;
+    valid = false;
+  } else {
+    valid = true;
+  }
+  return valid;
+}
+int main() {
+  int firstTest, secondTest, thirdTest;
+
+  getScore();
+  double avg = average (firstTest, secondTest, thirdTest);
+  char grade = getLetterGrade (avg, thirdTest);
+  cout << "Your course average is " << avg << endl;
+  cout << "Your final grade is " << grade << endl;
+
+  return 0;
+}
+
+
