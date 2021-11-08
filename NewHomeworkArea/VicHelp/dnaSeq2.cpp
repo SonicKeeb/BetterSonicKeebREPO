a12fd72326beec6fa4e28729fd7833a25bf92fc9
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-11-07 21:52:30 -0600

    fixed vicks format problem

--- NewHomeworkArea/VicHelp/dnaSeq2.cpp
+++ NewHomeworkArea/VicHelp/dnaSeq2.cpp
@@ -1,3 +1,12 @@
+//C++ SOURCE CODE:
+
+/*CovidDNASequence2.cpp: This Program Calculates The Amount, Percentage, 
+	and Type of Nucleotides from a Given Input File of a DNA Sequence 
+	(Covid Strain) and Outputs it into an Output File*/
+//Name: Victor Sim
+//Class Section: Online COSC 1436 (Programming Fundamentals 1)
+//Date: 11/6/21
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -6,86 +15,104 @@
 #include <cmath>
 using namespace std;
 
-int sumNucleotides = 0;
-int countAdenine = 0;
-int countThymine = 0;
-int countCytosine = 0;
-int countGuanine = 0;
-char charPlaceholder = 'z';
-const double PERCENTAGE_MAKER = 100;
-const int WIDTH_FORMAT = 11;
+int counting(string filename, int& sumNucleotides, int& countAdenine, int& countThymine, int& countCytosine, int& countGuanine) {
 
-bool counting() {
+	char charPlaceholder = 'z';
+	ifstream fileInputted;
+	fileInputted.open(filename);
+	if (fileInputted.fail()) {
+		cout << "Error: in opening input file\n";
+		return false;
+	}
 
-  ifstream fileInputted;
-  fileInputted.open("dnaSequence.txt");
-  if (fileInputted.fail()) {
-    cout << "Error: in opening input file\n";
-    return -1;
-  }
-
-  while (fileInputted >> charPlaceholder) {
-    if (charPlaceholder == 'A') { /*If The Char is an A, Increment count for Adenine and count for Total*/
-      countAdenine++;
-      sumNucleotides++;
-    } else if (charPlaceholder == 'T') { /*If the Char is an T, Increament count for Thymine and count for Total*/
-      countThymine++;
-      sumNucleotides++;
-    } else if (charPlaceholder == 'C') { /*If The Char is an C, Increment count for Cytosine and count for Total*/
-      countCytosine++;
-      sumNucleotides++;
-    } else if (charPlaceholder == 'G') { /*If The Char is an G, Increment count for Guanine and count for Total*/
-      countGuanine++;
-      sumNucleotides++;
-    } else { /*If Other Chars are found, it will print an error message*/ 
-      cout << "Error: " << charPlaceholder << " is not a valid character\n";
-      return false;
-    }
-  }
-
-
-fileInputted.close(); 
-return true;
-
+	while (fileInputted >> charPlaceholder) {
+		if (charPlaceholder == 'A') { /*If The Char is an A, Increment count for Adenine and count for Total*/
+			countAdenine++;
+			sumNucleotides++;
+		} else if (charPlaceholder == 'T') { /*If the Char is an T, Increament count for Thymine and count for Total*/
+			countThymine++;
+			sumNucleotides++;
+		} else if (charPlaceholder == 'C') { /*If The Char is an C, Increment count for Cytosine and count for Total*/
+			countCytosine++;
+			sumNucleotides++;
+		} else if (charPlaceholder == 'G') { /*If The Char is an G, Increment count for Guanine and count for Total*/
+			countGuanine++;
+			sumNucleotides++;
+		} else { /*If Other Chars are found, it will print an error message*/ 
+			cout << "Error: " << charPlaceholder << " is not a valid character\n";
+			return false;
+		}
+	}
+	fileInputted.close(); 
+	return true;
 }
 
-void format (ostream& output) {
+void formatLine(ostream& output, double countNucleotides, int amountOfSpecificNucleotide, char abbrevNucleotide) {
 
-  output << fixed << setprecision(2);
+	const double PERCENTAGE_MAKER = 100.00;
+	string nucleotide;
+	int countOfSpecificNucleotide = 0;
+	const int TWO_DECIMAL_FORMATTER(2);
+	const int WIDTH_FORMAT_ONE = 11;
+	const int WIDTH_FORMAT_TWO = 10;
 
-  output << setw(15) << left << setw(10) << left;
-  output << setw(15) << left << setw(10) << left;
-  output << setw(15) << left << setw(10) << left;
-  output << setw(15) << left << setw(10) << left;
+	if(abbrevNucleotide == 'A')
+	{
+		nucleotide = "Adenine";
+		countOfSpecificNucleotide = amountOfSpecificNucleotide;
+	}
+	else if(abbrevNucleotide =='T')
+	{
+		nucleotide = "Thymine";
+		countOfSpecificNucleotide = amountOfSpecificNucleotide;
+	}
+	else if(abbrevNucleotide == 'C')
+	{
+		nucleotide = "Cytosine";
+		countOfSpecificNucleotide = amountOfSpecificNucleotide;
+	}
+	else if(abbrevNucleotide =='G')
+	{
+		nucleotide = "Guanine";
+		countOfSpecificNucleotide = amountOfSpecificNucleotide;
+	}
 
+	if(nucleotide == "Adenine" || nucleotide == "Thymine" || nucleotide == "Guanine" ) { 
+		output << nucleotide << ":  " << setw(WIDTH_FORMAT_ONE) << left << countOfSpecificNucleotide << "\t";
+    output << fixed << setprecision(TWO_DECIMAL_FORMATTER) << countOfSpecificNucleotide / countNucleotides * PERCENTAGE_MAKER  <<"%\n";
+	}
+	else if(nucleotide == "Cytosine") {
+		output << nucleotide << ": " << setw(WIDTH_FORMAT_TWO) << left << countOfSpecificNucleotide << "\t" << fixed << setprecision(TWO_DECIMAL_FORMATTER) << countOfSpecificNucleotide / countNucleotides * PERCENTAGE_MAKER  <<"%\n";
+	}
 }
 
-void report (ostream& output) {
+void outputReport (ostream& outputFile, int sumNucleotides, int countAdenine, int countThymine, int countCytosine, int countGuanine) {
 
-  output <<"DNA sequence analysis:\n";
-  output << sumNucleotides << setw(6) << left << " nucleotides in the sequence\n";
-  output <<"\nSequence breakdown:\n"; 
-  output << "Adenine: " << countAdenine << " " << setw(7) << left << (double(countAdenine) / double(sumNucleotides)) * PERCENTAGE_MAKER << endl;   
-  output << "Thymine: " << countThymine << " " << setw(7) << left << (double(countThymine) / double(sumNucleotides)) * PERCENTAGE_MAKER << endl;
-  output << "Cytosine: " << countCytosine << " " << setw(7) << left << (double(countCytosine) / double(sumNucleotides)) * PERCENTAGE_MAKER << endl;
-  output << "Guanine: " << countGuanine << " " << setw(7) << left << (double(countGuanine) / double(sumNucleotides)) * PERCENTAGE_MAKER << endl;
+	char abbrevNucleotide;
+	outputFile << "DNA sequence analysis:\n" << sumNucleotides <<" nucleotides in the sentence.\n";
+	outputFile << "\nSequence breakdown:\n";
+	formatLine(outputFile, sumNucleotides, countAdenine, abbrevNucleotide = 'A');
+	formatLine(outputFile, sumNucleotides, countThymine, abbrevNucleotide = 'T');
+	formatLine(outputFile, sumNucleotides, countCytosine, abbrevNucleotide = 'C');
+	formatLine(outputFile, sumNucleotides, countGuanine, abbrevNucleotide = 'G');
 
 }
 
 int main() {
+	int sumNucleotides = 0;
+	int countAdenine = 0;
+	int countThymine = 0;
+	int countCytosine = 0;
+	int countGuanine = 0;
 
+	ofstream outputFile;
+	outputFile.open("covid_output.txt");
 
-  ofstream output;
-  output.open("FileToWrite.txt");
-  if (!output) {
-    cout << "Error in opening output file\n";
-    exit(0);
-  }
+	counting("dnaSequence.txt", sumNucleotides, countAdenine, countThymine, countCytosine, countGuanine);
 
-  counting();
-  format(output);
-  report(output);
+	outputReport(outputFile, sumNucleotides, countAdenine, countThymine, countCytosine, countGuanine);
 
-  return 0;
+	outputFile.close();
+
+	return 0;
 }
-
