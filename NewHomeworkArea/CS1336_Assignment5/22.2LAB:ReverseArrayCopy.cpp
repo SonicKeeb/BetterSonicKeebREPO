d4fcf44874d7093d33b0bcb98625c2eaf2796d06
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-11-10 09:28:52 -0600

    finished all assignment5

--- NewHomeworkArea/CS1336_Assignment5/22.2LAB:ReverseArrayCopy.cpp
+++ NewHomeworkArea/CS1336_Assignment5/22.2LAB:ReverseArrayCopy.cpp
@@ -4,9 +4,15 @@ using namespace std;
 /* Your function here */
 
 void reverseArrayCopy(int original[], int originalSize, int newVersion[], int newVersionSize) {
-  
-  
+  //In slides at the end, loop through array and input each element into new array
+  // so long as the new array is as large as the original you have room to input more
 
+  if (newVersionSize < originalSize) {
+    return;
+  }
+  for (int i = 0; i < originalSize; i++){
+    newVersion[i] = original[originalSize-1-i];
+  }
 }
 
 int main() {
@@ -18,6 +24,20 @@ int main() {
   }
 
   /* Your code here */
+
+  int size = sizeof(original)/sizeof(original[0]);
+
+  int newVersion[ARRAY_SIZE];
+
+  for (int i = size-1; i >= 0; i--) {
+    newVersion[i] = original[i];
+    cout << newVersion[i] << " ";
+  }
+
+  cout << endl;
+
+  reverseArrayCopy(original, ARRAY_SIZE, newVersion, ARRAY_SIZE);
+
 
   return 0;
 }
