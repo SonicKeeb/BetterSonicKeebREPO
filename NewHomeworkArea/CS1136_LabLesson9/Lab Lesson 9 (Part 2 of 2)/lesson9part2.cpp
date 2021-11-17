cbfb65f14dece56a41c826d540142e09b2ccc229
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2021-11-17 12:53:02 -0600

    just pushing to save

--- NewHomeworkArea/CS1136_LabLesson9/Lab Lesson 9 (Part 2 of 2)/lesson9part2.cpp
+++ NewHomeworkArea/CS1136_LabLesson9/Lab Lesson 9 (Part 2 of 2)/lesson9part2.cpp
@@ -1,36 +1,69 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <fstream>
 using namespace std;
 
-double interestRate() {
-  cout << "Enter annual interest rate" << endl;
-  double interest;
-  while (cin >> interest) {
-    if (interest <= 0) {
-      cout << "The annual interest rate must be greater than zero" << endl;
-      cout << "Enter annual interest rate" << endl;
+unsigned int read(unsigned int& rValue, istream& inputFile, double& presentValue, double& interestRate, int& months) {
+  while (inputFile >> presentValue >> interestRate >> months) {
+    if (presentValue <= 0 || interestRate <= 0 || months <= 0) {
+      cout << fixed << setprecision(2) << presentValue << " " << fixed << setprecision(3) << interestRate << " " << months << endl;
+      cout << "One or more of the above values are not greater than zero" << endl;
+      rValue = 2;
     } else {
-      return interest / 100;
+      interestRate = interestRate / 100;
+      rValue = 1;
     }
+  }
+  return rValue;
+}
+
+double futureValue(double presentValue, double interestRate, int months) {
+  double f = presentValue * pow((1 + interestRate), months);
+  return f;
+}
+
+void output(ostream& outputFile, istream& inputFile, unsigned int rValue, double presentValue, double interestRate, int months) {
+  outputFile << "Future Value\tPresent Value\tMonthly Interest\tMonths" << endl;
+  if (rValue != 1) {
+    return;
+  } else {
+    outputFile << fixed << setprecision(2) << futureValue(presentValue, interestRate, months);
+    outputFile << "\t" << fixed << setprecision(2) << presentValue << "\t" << fixed << setprecision(3) << interestRate * 100 << "\t" << months << endl;
   }
 }
 
-int numberYears() {
-  cout << "Enter number of years" << endl;
-  int num;
-  while (cin >> num) {
-    if (num <= 0) {
-      cout << "The number of years must be greater than zero" << endl;
-      cout << "Enter number of years" << endl;
-    } else {
-      return num;
-    }
-  }
-}
 
-double futureValue(double presentValue, double interestRate, int months)
 
 int main() {
+  double presentValue, interestRate;
+  int months;
+  unsigned int rValue;
+  string fileName;
+
+  cout << fixed << setprecision(2);
+  cout << "Enter input file name" << endl;
+  cin >> fileName;
+
+  ifstream inputFile;
+  inputFile.open(fileName);
+  if (!inputFile.is_open()) {
+    cout << "File " "\"" << fileName << "\" could not be opened" << endl;
+    exit(2);
+  }
+
+  ofstream outputFile;
+  outputFile.open("output.xls");
+  if (!outputFile.is_open()) {
+    cerr << "Output file didn't open!" << endl;
+    exit(1);
+  }
+
+  while (inputFile) {
+    read(rValue, inputFile, presentValue, interestRate, months);
+    futureValue(presentValue, interestRate, months);
+    output(outputFile, inputFile, rValue, presentValue, interestRate, months);
+  }
+
   return 0;
 }
