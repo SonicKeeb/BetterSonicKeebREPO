458601793f3ad7c61af27e02ffd6b1aeebf7ebaf
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2022-01-28 14:53:05 -0600

    for dillan

--- NewHomeworkArea/workspace/main.cpp
+++ NewHomeworkArea/workspace/main.cpp
@@ -1,22 +1,19 @@
+// This program takes in an input in fahrenheit and converts it into celsius
+// Dillan Nguyen
+// COSC 1436.900
+// 1/28/2022
 #include <iostream>
-using namespace std;
+using namespace std; // namespace std makes it so that I don't have to use std:: over and over again
 
 int main() {
 
-int decision;
+int fahrenheit, celsius; // Declaration of integer variables
 
-cout << "Choose choice 1" << endl;
-cout << "Enter with the number keys" << endl;
-cout << "1." << endl;
-cout << "2." << endl;
-cin >> decision;
-
-while (decision != 1) {
-  cout << "Choose again" << endl;
-  cin >> decision;
-}
-
-cout << "nice" << endl;
+cout << "Enter a temperature in fahrenheit "; // Output of prompt statement of the program
+cin >> fahrenheit; // Takes the input of the integer variable Fahrenheit
+celsius = (fahrenheit - 32) / 1.8; // Initializing celsius as the formula for making fahrenheit turn into celsius
+cout << fahrenheit << " degrees Fahrenheit = " << celsius << " degrees Celsius" << endl; // outputting computation
+// and output statement
 
   return 0;
 }
