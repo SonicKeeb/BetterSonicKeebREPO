939dcfc6541a40bd5e0c38e5a912102f44b7d082
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2022-09-06 09:47:25 -0500

    inclass activity submission

---
+++ CS2336/classActivities/pe2.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+  Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+  C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+  Code, Compile, Run and Debug online from anywhere in world.
+
+ *******************************************************************************/
+public class Main
+{
+  public static void main(String[] args) {
+    intRecursion(0);
+  }
+
+  public static int intRecursion(int num) {
+    // Base case? Nah, we don't need that!
+    int val1 + 100;
+    int val2 + 200;
+
+    System.out.println("Current value: " + num);
+
+    return intRecursion(num + 1);
+  }
+}
+
