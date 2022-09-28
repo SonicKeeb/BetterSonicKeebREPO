9d42fb46c7c65a89abe20c70857b8e93c001e290
Hamilton Ngo <59378286+SonicKeeb@users.noreply.github.com>
2022-09-28 10:48:33 -0500

    trying to fix swap issue

--- CS2336/projects/project1/Main.java
+++ CS2336/projects/project1/Main.java
@@ -16,8 +16,7 @@ public class Main {
     System.out.println("4. Quit");
     System.out.println("\n" + "What is your choice?");
   }
-  public static int displaySecond(int input) {
-    if (input == 1) {
+  public static void displaySecond() {
       System.out.println("Selected option 1: Query information through rewards members");
       System.out.println("\n" + "1. Miles accumulated in the current year");
       System.out.println("2. Total miles accumulated since joining the rewards program");
@@ -25,17 +24,11 @@ public class Main {
       System.out.println("4. Current reward tier(based on miles accumluated from the previous year");
       System.out.println("5. Reward tier of GIVEN prior year");
       System.out.println("4. EXIT");
-    }
-
-    if (input == 4) {
-      System.exit(1);
-    }
-    return input;
   }
   public static int milesAccumulatedCurrent(String[] date, int[] memberID, int[] flightMiles,
       int[] year, int length, int accumulated) {
     accumulated = 0;
-    System.out.println("What year? ");
+    System.out.println("What year? Put year first and then id");
     Scanner input = new Scanner(System.in);
     int yearChoice = input.nextInt();
     int ID = input.nextInt();
@@ -44,32 +37,45 @@ public class Main {
         accumulated += flightMiles[i];
       }
     }
+    System.out.println(accumulated);
     return accumulated;
   }
+  public static int totalMilesAccumulated(String[] date, int[] memberID, int[] flightMiles,
+  int[] year, int length, int overall) {
+    overall = 0;
+    System.out.println("Which member do you want to search, for the total accumulated miles");
+    Scanner input = new Scanner(System.in);
+    int ID = input.nextInt();
+   for (int i = 0; i < length; ++i) {
+      if (memberID[i] == ID) {
+        overall += flightMiles[i];
+      }
+    }
+    System.out.println(overall);
+    return overall; 
+  }
   public static void main(String[] args) throws IOException {
-    displayFirst(); 
-    Scanner myChoice = new Scanner(System.in);
-    int choice = myChoice.nextInt();
-    displaySecond(choice);
-
-    int counter = 0;
     FileInputStream inputFile = null;
     Scanner scnr = null;
     System.out.println("Opening file input.txt.");
     inputFile = new FileInputStream("input.txt");
     scnr = new Scanner(inputFile);
+    
+    int counter = 0;
+    int accumulated = 0;
+    int overall = 0;
 
     while (scnr.hasNextLine()) {
       counter = counter + 1;
       scnr.nextLine();
     }
-
     scnr.close();
+
     inputFile = new FileInputStream("input.txt");
     scnr = new Scanner(inputFile);
     int length = counter;
     System.out.println("Length of file: " + length);
-
+    
     String[] date = new String[length];
     int[] memberID = new int[length];
     int[] flightMiles = new int[length];
@@ -81,8 +87,26 @@ public class Main {
       flightMiles[i] = scnr.nextInt();
       year[i] = Integer.parseInt(date[i].substring(0,4));
     }
+    
+    Scanner myChoice = new Scanner(System.in);
+    int choice = 0;
+    while (choice == 0) {
+    displayFirst();
+    choice = myChoice.nextInt();
+    if (choice == 1) {
+    displaySecond();
+    choice = myChoice.nextInt();
+    if (choice == 1) {
+      milesAccumulatedCurrent(date, memberID, flightMiles, year, length, accumulated);
+    } else if (choice == 2) {
+      totalMilesAccumulated(date, memberID, flightMiles, year, length, overall);
+    }
+    } else if (choice == 4) {
+      System.edit(0);
+    }
 
-
+    choice = 0;
+}
 
     scnr.close();
     System.out.println("Closing file input.txt");
